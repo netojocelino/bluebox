575f28f373928e05800f77037108cf6e71a1a267
Jocelino Neto <netoj96@live.com>
2022-06-29 20:01:24 -0300

    first spec test

---
+++ index.spec.ts
@@ -0,0 +1,47 @@
+import Bluebox from "./index";
+import { MailData } from "~/types/Mail";
+import { must, asserts } from './src/tests/utils'
+import 'dotenv/config'
+
+const apiKey =  process.env!.SENDINBLUE_API as string;
+
+const timestamp = () => {
+  const now = (new Date()).toLocaleString('pt-BR').split(' ')
+  return `${now[1]} de ${now[0]}`
+}
+
+const emailBody: MailData = {
+  to: [{
+    name: 'Jocelino',
+    email: 'netoj96@live.com',
+  }],
+  sender: {
+    name: 'Neto Jocelino',
+    email: 'netoj96@live.com',
+  },
+  subject: 'Sending a email with bluebox (a Sendinblue wrapper)',
+  // body: 'Do you are seeing this?',
+  // templateId: 3,
+  htmlContent: `<div>Enviado em ${timestamp()}</div>`
+}
+
+
+must("Bluebox → Must have api_key", function () {
+  asserts(apiKey).notNull()
+});
+
+must("Bluebox → Must instance with api_key", function () {
+  const bluebox = new Bluebox(apiKey)
+});
+
+must("Bluebox → Must send a email", async function () {
+  const bluebox = new Bluebox(apiKey)
+  const response:any = await bluebox.send(emailBody);
+
+  asserts(response).notNull();
+
+  asserts(response!.status).equals(201)
+}, {
+  type: 'Boxblue',
+  message: 'Boxblue'
+});
